387822df4cd0c632fa8f2d9da9de51022d830bcf
Carey <clh@bupt.edu.cn>
2023-05-19 22:05:32 +0800

    Revert "second commit"
    This reverts commit 045645ad

--- gitTest.java
+++ gitTest.java
@@ -2,6 +2,5 @@ public class gitTest {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("second version");
-        System.out.println("third version");
     }
 }
