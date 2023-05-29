087fc78aa789cf407d6a9592f22735c896759042
Carey <clh@bupt.edu.cn>
2023-05-29 17:58:39 +0800

    "my new commit"

--- gitTest.java
+++ gitTest.java
@@ -3,5 +3,6 @@ public class gitTest {
         System.out.println("Hello world");
         System.out.println("second version");
         System.out.println("third version");
+        System.out.println("new version");
     }
 }
